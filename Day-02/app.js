c8e09d66d5244cac6695ca570dce53a5b0953d98
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2026-07-16 12:57:30 +0500

    Day 2 about REPL Process Argv and a mini cli application

---
+++ Day-02/app.js
@@ -0,0 +1,19 @@
+// console.log(global)
+
+//Node js version
+// console.log(process.version)
+
+//Current working directory
+// console.log(process.cwd())
+
+// Platform
+// console.log(process.platform)
+
+// Architecture
+// console.log(process.arch)
+
+// Argument vector 
+//  console.log(process.argv)
+
+const name = process.argv[2];
+console.log(`Hello ${name}. Welcome to Node Js`)
