9c424d1d3c61b45f39a1a3d3c5fb27a784fd61a7
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2026-07-17 13:03:38 +0500

    create a new moduel for user

--- Day-03/app.js
+++ Day-03/app.js
@@ -1,7 +1,11 @@
 const calculator = require("./Modules/math");
+const user = require("./Modules/user")
 
 console.log(calculator.add(20, 30));
 
 console.log(calculator.multiply(20 , 5))
 
 console.log(calculator.subtract(100,30))
+
+
+console.log(user.name)
